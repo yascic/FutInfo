251709e32c1a3705b9d4f142a5ded2c8010fe19d
Yahya Ascic <yahya.ascic07@gmail.com>
2022-03-20 19:39:12 +0100

    Initial Commit

---
+++ FutInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FutInfo
+//
+//  Created by Yahya Aščić on 20.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
